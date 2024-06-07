0f039fe967261665a9b8172cd39041a3433887fb
Jeremy <ogprago@gmail.com>
2024-06-07 11:16:42 -0400

    Added inventory, sales_contracts, and lease_contracts tables

--- setup.sql
+++ setup.sql
@@ -1,10 +1,11 @@
+-- Create database
 DROP DATABASE IF EXISTS CarDealershipDatabase;
 
 CREATE DATABASE IF NOT EXISTS CarDealershipDatabase;
 
 USE CarDealershipDatabase;
 
-
+-- dealerships table
 CREATE TABLE `dealerships`(
     `dealership_id` int AUTO_INCREMENT,
     `name` varchar(50) NOT NULL,
@@ -13,8 +14,31 @@ CREATE TABLE `dealerships`(
     PRIMARY KEY(`dealership_id`)
 );
 
+-- vehicles table
 CREATE TABLE `vehicles`(
 	`VIN` int NOT NULL, 
     `SOLD` varchar(9) NOT NULL, 
     PRIMARY KEY(`VIN`)
+);
+
+-- inventory table
+CREATE TABLE `inventory`(
+	`dealership_id` int NOT NULL,
+    `VIN` int NOT NULL
+);
+
+-- sales_contracts table
+CREATE TABLE `sales_contracts`(
+	`sales_id` int AUTO_INCREMENT,
+    `VIN` int NOT NULL,
+	PRIMARY KEY (`sales_id`),
+    FOREIGN KEY (`VIN`) REFERENCES vehicles(`VIN`)
+);
+
+-- lease_contracts table
+CREATE TABLE `lease_contracts`(
+	`lease_id` int AUTO_INCREMENT,
+	`VIN` int NOT NULL,
+	PRIMARY KEY (`lease_id`),
+	FOREIGN KEY (`VIN`) REFERENCES vehicles(`VIN`)
 );
